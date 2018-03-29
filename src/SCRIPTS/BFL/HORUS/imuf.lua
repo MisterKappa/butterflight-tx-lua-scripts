ce5cfedce9cc2a0f8bab9a5f997932204d932393
papayou66 <alainwalter2805@gmail.com>
2018-03-29 10:41:57 +0200

    HORUS Layout (need testing)

--- src/SCRIPTS/BFL/HORUS/imuf.lua
+++ src/SCRIPTS/BFL/HORUS/imuf.lua
@@ -3,28 +3,32 @@ return {
     read              = 227, -- MSP_IMUF_CONFIG
     write             = 228, -- MSP_SET_IMUF_CONFIG
     eepromWrite       = true,
-    reboot            = true,
+    reboot            = false,
     title             = "Imuf",
     minBytes          = 20,
-    text= {
-      { t = "Q", x = 50, y = 14, to = SMLSIZE },
-      { t = "W", x = 80, y = 14, to = SMLSIZE },
-      { t = "LPF", x = 105, y = 14, to = SMLSIZE },
-      { t = "ROLL", x = 10, y = 24, to = SMLSIZE },
-      { t = "PITCH", x = 10, y = 34, to = SMLSIZE },
-      { t = "YAW", x = 10, y = 44, to = SMLSIZE },
-      { t = "MODE", x = 140, y = 24, to = SMLSIZE },
-    },
-    fields = {
-      { x = 45,  y = 24, min = 0, max = 16000, to = SMLSIZE, vals = { 7, 8 } },
-      { x = 75,  y = 24, min = 0, max = 16000, to = SMLSIZE, vals = { 9, 10 } },
-      { x = 45,  y = 34, min = 0, max = 16000, to = SMLSIZE, vals = { 3, 4 } },
-      { x = 75,  y = 34, min = 0, max = 16000, to = SMLSIZE, vals = { 5, 6 } },
-      { x = 45,  y = 44, min = 0, max = 16000, to = SMLSIZE, vals = { 11, 12 } },
-      { x = 75,  y = 44, min = 0, max = 16000, to = SMLSIZE, vals = { 13, 14 } },
-      { x = 105, y = 24, min = 0, max = 255, to = SMLSIZE, vals = { 17, 18 } },
-      { x = 105, y = 34, min = 0, max = 255, to = SMLSIZE, vals = { 15, 16 } },
-      { x = 105, y = 44, min = 0, max = 255, to = SMLSIZE, vals = { 19, 20 } },
-      { x = 165, y = 24, min = 0, max = 255, to = SMLSIZE, vals = { 1, 2 } },
-	},
+    text = {
+      { t = "Q",     x = 142,  y =  48, to = MIDSIZE },
+      { t = "W",     x = 244,  y =  48, to = MIDSIZE },
+      { t = "LPF",   x = 342,  y =  48, to = MIDSIZE },
+      { t = "ROLL",  x =  28,  y = 100 },
+      { t = "PITCH", x =  28,  y = 132 },
+      { t = "YAW",   x =  28,  y = 164 },
+      { t = "MODE",  x =  28,  y = 200 },
+  },
+   fields = {
+      -- Q
+      { x = 140, y = 100, min = 0, max = 16000, vals = { 7, 8 }, to = MIDSIZE },
+      { x = 140, y = 132, min = 0, max = 16000, vals = { 3, 4 }, to = MIDSIZE },
+      { x = 140, y = 164, min = 0, max = 16000, vals = { 11, 12 }, to = MIDSIZE },
+      -- W
+      { x = 240, y = 100, min = 0, max = 16000, vals = { 9, 10 }, to = MIDSIZE },
+      { x = 240, y = 132, min = 0, max = 16000, vals = { 5, 6 }, to = MIDSIZE },
+      { x = 240, y = 164, min = 0, max = 16000, vals = { 13, 14 }, to = MIDSIZE },
+      -- LPF
+      { x = 340, y = 100, min = 0, max = 255, vals = { 17, 18 }, to = MIDSIZE },
+      { x = 340, y = 132, min = 0, max = 255, vals = { 15, 16 }, to = MIDSIZE },
+      { x = 440, y = 164, min = 0, max = 255, vals = { 19, 20 }, to = MIDSIZE },
+      -- MODE
+      { x = 200, y = 200, min = 0, max = 255, vals = { 1, 2 }, to = MIDSIZE },	  
+  },
 }
--- src/SCRIPTS/BFL/HORUS/kalman.lua
+++ src/SCRIPTS/BFL/HORUS/kalman.lua
@@ -6,9 +6,12 @@ return {
    reboot         = false,
    eepromWrite    = true,
    minBytes       = 4,
-   text = {},
+   text = {
+    { t = "Kalman Q",  x =  28,  y = 100 },
+    { t = "Kalman R",  x =  28,  y = 150 },
+   },
    fields = {
-    { t = "Kalman Q",  x =  10, y = 24, sp = 45, min = 0, max = 16000, vals = { 1, 2 }, mult = 100,to = SMLSIZE },
-    { t = "Kalman R",  x =  10, y = 44, sp = 45, min = 0, max = 16000, vals = { 3, 4 }, mult = 1,to = SMLSIZE },
-  },
+    { x = 240, y = 100, min = 0, max = 200, vals = { 1, 2 }, mult = 100, to = MIDSIZE },
+    { x = 240, y = 150, min = 0, max = 200, vals = { 3, 4 }, to = MIDSIZE },
+   },
 }
